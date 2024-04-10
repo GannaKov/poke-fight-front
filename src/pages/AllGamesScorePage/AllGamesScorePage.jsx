d4d9a8f79e502b56fbff9da9ee6272ad308341ef
GannaKov <zlatta2000@gmail.com>
2024-04-10 23:04:03 +0200

    style statistics

--- src/pages/AllGamesScorePage/AllGamesScorePage.jsx
+++ src/pages/AllGamesScorePage/AllGamesScorePage.jsx
@@ -1,29 +1,39 @@
 import { useLoaderData } from "react-router-dom";
+import styles from "./AllGamesScorePage.module.css";
 
 const AllGamesScorePage = () => {
   const allGamesResult = useLoaderData();
 
   return (
-    <div>
-      <p> Statistics</p>
-      <ul>
-        {" "}
-        {allGamesResult.map(
-          ({ participents, pokWinner, winner, score, _id }) => (
-            <li key={_id}>
-              <p>
-                {participents.pok1.name}&nbsp;:&nbsp;{participents.pok2.name}
-              </p>
-              <p>
-                Score:&nbsp;{score[0]}&nbsp;:&nbsp;{score[1]}
-              </p>
-              <p>
-                Winner:&nbsp;{winner} with pokemon {pokWinner.name}
-              </p>
-            </li>
-          )
-        )}
-      </ul>
+    <div className={styles.pageInnWrp}>
+      <h2 className={styles.pageTitle}> Statistics</h2>
+      {allGamesResult.length > 0 ? (
+        <ul>
+          {allGamesResult.map(
+            ({ participents, pokWinner, winner, score, _id }) => (
+              <li key={_id} className={styles.scoreItem}>
+                <p className={styles.itemTextTytle}>
+                  {participents.pok1.name}&nbsp;:&nbsp;{participents.pok2.name}
+                </p>
+                <p className={styles.itemText}>
+                  <span className={styles.itemSpan}>Score:</span> &nbsp;
+                  {score[0]}&nbsp;:&nbsp;{score[1]}
+                </p>
+                <p>
+                  <span className={styles.itemSpan}>Winner:</span>&nbsp;&nbsp;
+                  {winner}
+                  &nbsp; with pokemon
+                  <span className={styles.itemSpan}> {pokWinner.name}</span>
+                </p>
+              </li>
+            )
+          )}
+        </ul>
+      ) : (
+        <div>
+          <p>You haven&#x2bc;t played yet. Do it!</p>
+        </div>
+      )}
     </div>
   );
 };
